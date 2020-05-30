21eecc5dd757187d3fa165941d358953974c970e
chiawei2020 <chiawei861021@gmail.com>
2020-05-30 12:22:01 +0800

    change1 commit

--- random-select/data.js
+++ random-select/data.js
@@ -1,5 +1,5 @@
 let pictures=[
-    "https://pic.pimg.tw/smi1014/1536138689-693220624.jpg",
+    "https://s.yimg.com/zp/MerchandiseImages/A566CA2846-SP-7030667.jpg",
     "https://img.tiffany0118.com/uploads/20200113200825_91.jpg",
     "https://www.siammore.com.tw/images/2019/0102.jpg"
 ];
