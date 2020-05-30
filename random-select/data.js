a296bccb35f5ed6fa34e04a3985405428ffa7557
chiawei2020 <chiawei861021@gmail.com>
2020-05-30 12:16:46 +0800

    123change commit

--- random-select/data.js
+++ random-select/data.js
@@ -1,4 +1,5 @@
 let pictures=[
-    "https://pic.pimg.tw/summuslovecyndi/1480233261-2902284461.jpg",
+    "https://s.yimg.com/zp/MerchandiseImages/A566CA2846-SP-7030667.jpg",
     "https://img.tiffany0118.com/uploads/20200113200825_91.jpg",
-    "https://www.siammore.com.tw/images/2019/0102.jpg"];
+    "https://www.siammore.com.tw/images/2019/0102.jpg"
+];
